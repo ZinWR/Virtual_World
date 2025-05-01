ad962d5660b956612be75e9b1509e958a3ba7d14
QuanN <jeanquan98@gmail.com>
2025-04-30 22:18:04 -0500

    add graph functionality

--- js/math/graph.js
+++ js/math/graph.js
@@ -19,4 +19,62 @@ class Graph {
             point.draw(ctx);
         }
     }
+
+    addPoint(point) {
+        this.points.push(point);
+    }
+
+    containsPoint(point) {
+        return this.points.find(p => p.equals(point));
+    }
+
+    tryAddPoint(point) {
+        if (!this.containsPoint(point)) {
+            this.addPoint(point);
+            return true;
+        } 
+        return false;
+    }
+
+    addSegment(segment) {
+        this.segments.push(segment);
+    }
+
+    tryAddSegment(segment) {
+        if (!this.containsSegment(segment) && !segment.p1.equals(segment.p2)) {
+            this.addSegment(segment);
+            return true;
+        }
+        return false;
+    }
+
+    containsSegment(segment) {
+        return this.segments.find(s => s.equals(segment));
+    }
+
+    removeSegment(segment) {
+        this.segments.splice(this.segments.indexOf(segment), 1);
+    }
+    
+    removePoint(point) {
+        const segments = this.getSegmentsWithPoint(point);
+        for (const segment of segments) {
+            this.removeSegment(segment);
+        }
+        this.points.splice(this.points.indexOf(point), 1);
+    }
+
+    getSegmentsWithPoint(point) {
+        const segments = [];
+        for (const segment of this.segments) {
+            if (segment.includes(point)) segments.push(segment);
+        }
+        return segments;
+    }
+
+    dispose() {
+        this.points.length = 0;
+        this.segments .length = 0;
+    }
+
 }
