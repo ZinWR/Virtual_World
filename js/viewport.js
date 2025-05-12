5b49c10d573a03c7b4d3a404e518f222345ede0f
QuanN <jeanquan98@gmail.com>
2025-05-12 17:34:50 -0500

    implement zoom and edit graphEditor.js

--- js/viewport.js
+++ js/viewport.js
@@ -12,6 +12,13 @@ class Viewport {
     }
 
     #handleMouseWheel(event) {
-        console.log(event.deltaY);
+        const direction = Math.sign(event.deltaY);
+        const step = 0.1;
+        this.zoom += direction * step;
+        this.zoom = Math.max(1, Math.min(5, this.zoom)); // Limit zoom level between 1 and 5
+    }
+
+    getMouse(event) {
+        return new Point(event.offsetX * this.zoom, event.offsetY * this.zoom);
     }
 }
