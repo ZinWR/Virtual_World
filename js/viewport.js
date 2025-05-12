ced1ed56056dd01f6e9eee3e3510918011ec80c4
QuanN <jeanquan98@gmail.com>
2025-05-12 17:18:20 -0500

    start building viewport and zoom

---
+++ js/viewport.js
@@ -0,0 +1,17 @@
+class Viewport {
+    constructor(canvas) {
+        this.canvas = canvas;
+        this.ctx = canvas.getContext('2d');
+        this.zoom = 1;
+
+        this.#addEventListeners();
+    }
+
+    #addEventListeners() {
+        this.canvas.addEventListener('mousewheel', this.#handleMouseWheel.bind(this)); 
+    }
+
+    #handleMouseWheel(event) {
+        console.log(event.deltaY);
+    }
+}
