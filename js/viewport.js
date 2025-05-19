46248fbb61b73f1bd0820996a73e9a29dda6c290
QuanN <jeanquan98@gmail.com>
2025-05-19 18:29:02 -0500

    building drag offset

--- js/viewport.js
+++ js/viewport.js
@@ -3,12 +3,22 @@ class Viewport {
         this.canvas = canvas;
         this.ctx = canvas.getContext('2d');
         this.zoom = 1;
+        this.offset = new Point(0, 0);
+        this.drag = {
+            start: new Point(0, 0),
+            end: new Point(0, 0),
+            offset: new Point(0, 0),
+            active: false
+        };
 
         this.#addEventListeners();
     }
 
     #addEventListeners() {
         this.canvas.addEventListener('mousewheel', this.#handleMouseWheel.bind(this)); 
+        this.canvas.addEventListener('mousedown', this.#handleMouseDown.bind(this)); 
+        this.canvas.addEventListener('mousemove', this.#handleMouseMove.bind(this)); 
+        this.canvas.addEventListener('mouseup', this.#handleMouseUp.bind(this)); 
     }
 
     #handleMouseWheel(event) {
@@ -18,7 +28,35 @@ class Viewport {
         this.zoom = Math.max(1, Math.min(5, this.zoom)); // Limit zoom level between 1 and 5
     }
 
+    #handleMouseDown(event) {
+        // Middle Button
+        if (event.button === 1) {
+            this.drag.start = this.getMouse(event);
+            this.drag.active = true;
+        }
+    }
+
+    #handleMouseMove(event) {
+        if (this.drag.active) {
+            this.drag.end = this.getMouse(event);
+            this.drag.offset = subtract(this.drag.end, this.drag.start);
+        }
+    }
+
+    #handleMouseUp(event) {
+        if (this.drag.active) {
+            this.offset = add(this.offset, this.drag.offset);
+            this.drag = {
+                start: new Point(0, 0),
+                end: new Point(0, 0),
+                offset: new Point(0, 0),
+                active: false
+            };
+        }
+    }
+
     getMouse(event) {
         return new Point(event.offsetX * this.zoom, event.offsetY * this.zoom);
     }
+
 }
